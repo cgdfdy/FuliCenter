5b1f55cd158b503b08200138cb7fdbd8d063b659
cgdfdy <877491315@qq.com>
2016-10-18 21:26:17 +0800

    工程中产生的bug修正

--- app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailActivity.java
@@ -11,6 +11,7 @@ public class GoodsDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_detail);
        int goodsID = getIntent().getIntExtra(I.GoodsDetails.KEY_GOODS_ID,0);
