8bb054c2ebacea9c3513d29ee8aee5faf41df720
cgdfdy <877491315@qq.com>
2016-10-19 14:19:10 +0800

    添加精选布局文件

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/BoutiqueAdapter.java
@@ -0,0 +1,77 @@
+package cn.ucai.fulicenter.adapter;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.BoutiqueBean;
+
+/**
+ * Created by Administrator on 2016/10/19.
+ */
+public class BoutiqueAdapter extends Adapter {
+    Context context;
+    ArrayList<BoutiqueBean> mList;
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new GoodsAdapter.FooterViewHolder(LayoutInflater.from(context)
+                    .inflate(R.layout.item_footer, parent, false));
+        } else {
+            holder = new ButiqueViewHolder(LayoutInflater.from(context)
+                    .inflate(R.layout.item_boutique, parent, false));
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() + 1 : 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        }
+        return I.TYPE_ITEM;
+    }
+
+  class ButiqueViewHolder extends ViewHolder{
+        @Bind(R.id.ivBoutiqueImg)
+        ImageView ivBoutiqueImg;
+        @Bind(R.id.tvBoutiqueTitle)
+        TextView tvBoutiqueTitle;
+        @Bind(R.id.tvBoutiqueName)
+        TextView tvBoutiqueName;
+        @Bind(R.id.tvBoutiqueDescription)
+        TextView tvBoutiqueDescription;
+        @Bind(R.id.layout_boutique_item)
+        RelativeLayout layoutBoutiqueItem;
+
+       ButiqueViewHolder(View view) {
+           super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
