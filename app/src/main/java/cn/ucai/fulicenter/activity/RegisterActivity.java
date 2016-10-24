4d12edc240fc97a14632bbfcf8a304b0a8cfc998
cgdfdy <877491315@qq.com>
2016-10-24 18:01:17 +0800

    注册的框架搭建完成

--- app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/RegisterActivity.java
@@ -1,19 +1,38 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.view.DisplayUtils;
 
 public class RegisterActivity extends BaseActivity {
 
+    @Bind(R.id.username)
+    EditText username;
+    @Bind(R.id.nick)
+    EditText nick;
+    @Bind(R.id.password)
+    EditText password;
+    @Bind(R.id.confirm_Password)
+    EditText confirmPassword;
+    @Bind(R.id.btn_register)
+    Button btnRegister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     protected void initView() {
+        DisplayUtils.initBackWithTitle(this,"账户注册");
 
     }
 
