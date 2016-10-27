ed2b8deb296564ecadaae1fe12a39fb3a6e9c072
cgdfdy <877491315@qq.com>
2016-10-27 16:48:35 +0800

    json转换为购物车实体类的方法

--- app/src/main/java/cn/ucai/fulicenter/adapter/CartAdapter.java
+++ app/src/main/java/cn/ucai/fulicenter/adapter/CartAdapter.java
@@ -18,17 +18,19 @@ import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.bean.CartBean;
+import cn.ucai.fulicenter.bean.GoodsDetailsBean;
+import cn.ucai.fulicenter.utils.ImageLoader;
 
 /**
  * Created by Administrator on 2016/10/19.
  */
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
-    Context context;
+    Context mContext;
     ArrayList<CartBean> mList;
 
 
     public CartAdapter(Activity context, ArrayList<CartBean> mList) {
-        this.context = context;
+        this.mContext = context;
         this.mList = new ArrayList<>();
         this.mList.addAll(mList);
     }
@@ -37,19 +39,23 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
     @Override
     public CartViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         CartViewHolder holder = null;
-        holder = new CartViewHolder(LayoutInflater.from(context).inflate(R.layout.item_cart, parent, false));
+        holder = new CartViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_cart, parent, false));
         return holder;
     }
 
     @Override
     public void onBindViewHolder(CartViewHolder holder, int position) {
-        CartBean boutique = mList.get(position);
-     /*   ImageLoader.downloadImg(context, holder.ivBoutiquePicture, boutique.getImageurl());
-        holder.tvBoutiqueTitle.setText(boutique.getTitle());
-        holder.tvBoutiqueName.setText(boutique.getName());
-        holder.tvBoutiqueDesc.setText(boutique.getDescription());*/
-
+        CartBean cartBean = mList.get(position);
+        GoodsDetailsBean goods = cartBean.getGoods();
+        if (goods!=null) {
+            ImageLoader.downloadImg(mContext, holder.mIvCartThumb, goods.getGoodsThumb());
+            holder.mIvCartGoodName.setText(goods.getGoodsName());
+            holder.mTvCartPrice.setText(goods.getCurrencyPrice());
+        }
+        holder.mTvCartCount.setText(cartBean.getCount());
+        holder.mCbCartSelected.setChecked(false);
     }
+
 
     @Override
     public int getItemCount() {
